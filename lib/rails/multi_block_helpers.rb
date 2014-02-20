db5384d8f8eabdb7973d2a69516651094b969e66
stevo <blazejek@gmail.com>
2014-02-20 11:04:42 +0100

    remove obsolete puts ;P

--- lib/rails/multi_block_helpers.rb
+++ lib/rails/multi_block_helpers.rb
@@ -1,4 +1,3 @@
-puts '-------------------------------------'
 require 'rails/multi_block_helpers/switch_block'
 require 'rails/multi_block_helpers/capture_section_helper'
 require 'rails/multi_block_helpers/railtie' if defined?(Rails)
